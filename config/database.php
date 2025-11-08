522277765fe44c8a0a0a24e940acd8de78598421
Remy Rivera <remyrivera@gmail.com>
2025-11-08 11:11:52 -0500

    Actualizacion de libreria

--- config/database.php
+++ config/database.php
@@ -107,10 +107,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
-            'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
+	],
 
     ],
 
